e05bca429667605f67ce81dcf34d327fcd6e93f9
Thiha Zaw Htet <thihazawhtet214@gmail.com>
2021-06-05 10:42:06 +0630

    header,footer and login page done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 mr-5 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
+    <ul class="nav navbar-nav navbar-right mr-4">
+    	<li class="nav-item">
+	    	<a href="#" title="" class="nav-link">Cart(0)</a>
+    	</li>
+    </ul>
+  </div>
+</nav>
